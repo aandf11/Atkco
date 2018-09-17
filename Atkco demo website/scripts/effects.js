2efaf45f2efb7848023e15eae33d7ea958d1a81d
mr-ogodo <33297937+mr-ogodo@users.noreply.github.com>
2018-09-17 10:23:02 -0500

    Add files via upload
    homepage updates.
    tablet/ mobile views fully functional

--- Atkco demo website/scripts/effects.js
+++ Atkco demo website/scripts/effects.js
@@ -1,1 +1,12 @@
-// JavaScript Document
+function activateAnimation(){
+	$("#mobile-menu").click(function(){
+		$(".mobile-menu-content").addClass("mobile-menu-content-active");
+		$(".close-menu").removeClass("mobile-menu-content-active");
+		});
+	$(".close-menu").click(function(){
+		$(".mobile-menu-content").removeClass("mobile-menu-content-active");
+		});
+	}
+
+
+$(document).ready(activateAnimation);
