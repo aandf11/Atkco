35064f50028cd1c6f4407c9fd85c89ddb8c90ad1
mr-ogodo <33297937+mr-ogodo@users.noreply.github.com>
2018-10-15 14:40:03 -0500

    Add files via upload
    added sign up section to login page.
    fully responsive.
    with transition animation

--- Atkco demo website/scripts/effects.js
+++ Atkco demo website/scripts/effects.js
@@ -6,6 +6,15 @@ function activateAnimation(){
 	$(".close-menu").click(function(){
 		$(".mobile-menu-content").removeClass("mobile-menu-content-active");
 		});
+	/*SIGNUP FORM DISPLAY*/
+	$("#btn-signup").click(function(){
+		$("#form1").fadeOut(100);
+		$("#signupForm").fadeIn(90);
+		});
+	$("#cancel-btn").click(function(){
+		$("#signupForm").fadeOut(100);
+		$("#form1").fadeIn(80);
+		});
 	}
 
 
