f0972aa74d2bcab8359b968a2671b5635a6bd3f1
mr-ogodo <33297937+mr-ogodo@users.noreply.github.com>
2018-10-15 21:23:23 -0500

    Add files via upload

--- Atkco demo website/scripts/effects.js
+++ Atkco demo website/scripts/effects.js
@@ -1,4 +1,5 @@
 function activateAnimation(){
+	/*MOBILE MENU DISPLAY*/
 	$("#mobile-menu").click(function(){
 		$(".mobile-menu-content").addClass("mobile-menu-content-active");
 		$(".close-menu").removeClass("mobile-menu-content-active");
@@ -9,11 +10,22 @@ function activateAnimation(){
 	/*SIGNUP FORM DISPLAY*/
 	$("#btn-signup").click(function(){
 		$("#form1").fadeOut(100);
-		$("#signupForm").delay(90).fadeIn(90);
+		$("#signupForm").delay(80).fadeIn(90);
 		});
 	$("#cancel-btn").click(function(){
 		$("#signupForm").fadeOut(60);
-		$("#form1").delay(90).fadeIn(80);
+		$("#form1").delay(70).fadeIn(80);
+		});
+	/*QUESTIONS DISPLAY*/
+	$("#start-assesment").click(function(){
+		$("#tips-background").fadeOut();
+		$("#tips-screen").fadeOut();
+		$(".questions-container").fadeIn(10);
+		});
+	$(".close-btn").click(function(){
+		$("#tips-background").fadeIn();
+		$("#tips-screen").fadeIn();
+		$(".questions-container").fadeOut(10);
 		});
 	}
 
